ac8ccede456d9b62a2516974431d50dc592c7563
Firdaus Saad <firdaussaad@gmail.com>
2021-04-27 12:00:41 +0800

    second python file added

---
+++ part3-working_with_numbers.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3.6
+
+# % give the remainder
+# str convert the number into string
+# abs convert the number into absolute. -5 to 5
+# pow to the power of the number
+# max show us the biggest number
+# min show us the smallest number
+# round. increase up or down to whole number
+# floor - round down
+# ceil - round up
+
+my_num = 2
+num2 = 4
+
+print(my_num + num2)
+
+
+
+
+print(str(my_num) + " is my favorite number")
+
+print(abs(-5))
+print(pow(5, 6))
+
+print(min(1, 10, 20, 5, 60))
+print(max(1, 10, 20, 5, 60))
+
+print(round(3.5))
+
+from math import *
+
+print(floor(3.9))
+print(ceil(3.1))
+print(sqrt(36))
