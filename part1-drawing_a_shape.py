39d64266d0be3a43c5c2c2c8689d37ca8e6367df
Firdaus Saad <firdaussaad@gmail.com>
2021-04-27 11:19:14 +0800

    first python files

---
+++ part1-drawing_a_shape.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3.6
+
+print('Hello World')
+
+print("   /|")
+print("  / |")
+print(" /  |")
+print("/___|")
---
+++ part2-variable_and_data_type.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3.6
+
+# strings are just plain text. Only need "" when storing a string.
+# bullion value is a true or false value
+# can only concatenate str (not "int") to str. Add str
+
+character_name = "John"
+character_age = 35
+is_male = True
+print("There was once a man name " + character_name + ",")
+print("He was " + str(character_age) + "years old")
+
+character_name = "Mike"
+print("He really liked the name " + character_name + ", ")
+print("but didn,t like being " + str(character_age))
